15380e8afa4bb7f4449a9b33f607c19529b93762
paulibrignole <paulamorena.b@gmail.com>
2023-11-02 18:33:34 -0300

    subida de la segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,94 +1,104 @@
+const camperaCompra = [
+    { Opcion:1, nombre: "Campera Flower", precio: 35000},
+    { Opcion:2, nombre: "Campera Frida", precio: 40000},
+    { Opcion:3, nombre: "Campera Freddie", precio: 40000},
+    { Opcion:4, nombre: "Campera Cat", precio: 45000},
+    { Opcion:5, nombre: "Campera Love", precio: 45000},
 
-// Se trata de un emprendimiento de chaquetas pintadas y customizadas a mano, donde ayudamos al cliente a que se diseñe su propia campera.
+]
 
+let shoppingCart = [];
 
-let Bienvenida = prompt ("Bienvenido a Diseña Tu Chaqueta, vamos a hacer una serie de preguntas para conseguir tu chaqueta ideal pintada a mano, selecciona la opcion deseada \n1: Comenzar a diseñar \n2: Salir")
+let selection = prompt("Hola! Te damos la bienvenida a Ara Morena, camperas personalizadas. Esta es nuestra nueva colección. Deseas comprar? escribe: si para continuar y no para salir");
 
-function crearCampera () {
-    let materialCampera = prompt ("ingrese el tipo de material a utilizar, -Denim pesado o -Denim ligero")
-console.log("su chaqueta va a ser de"+ materialCampera)
-
-let ubicacionCampera = prompt ("¿Escribe la opcion deseada. Deseas que este pintada en -el frente y la espalda, -el frente o -la espalda?")
-console.log("su chaqueta va a ser de"+ materialCampera + "va a estar pintada en"+ ubicacionCampera)
-
-let colorCampera = prompt ("Selecciona el color de tu campera - Negra - azul - celeste")
-console.log("Su chaqueta va a ser de: "+ materialCampera + "va a estar pintada en:" + ubicacionCampera + "y sera de color "+ colorCampera)
-
-let talleCampera = prompt ("¿Cual es su talle? - s - m - l -xl -xxl -xxxl ")
-console.log("Su chaqueta va a ser de: "+ materialCampera + "va a estar pintada en:" + ubicacionCampera + "sera de color "+ colorCampera + "y de talle"+ talleCampera)
+while(selection != "si" && selection != "no"){
+    alert("Por favor ingrese el si o no")
+    seleccionar = prompt("Deseas comprar: si o no?")
 }
 
+if(selection === "si"){
 
-function finalizandoCampera () {
+    alert("A continuacion vas a ver las camperas disponibles");
+    let listaProductos = camperaCompra.map(
+        (producto) => producto.Opcion + "." + producto.nombre + " " +  "$" + producto.precio
+    );
 
-    let tematicaCampera = prompt ("¿Cual de las siguientes tematicas te gustaria que tenga tu chaqueta? \n1: Peliculas o series \n2: Un artista o banda \n3: Solo tipografia \n4: Naturaleza \n5: Psicoldelica \n6: Dibujos animados \n7: Animales \n8: Tengo mi diseño")
-    console.log("Tu chaqueta tendra la tematica de:"+ tematicaCampera)
+    alert(listaProductos.join("/"));   
 
-    switch (tematicaCampera) {
-        case "Peliculas o series": 
-            alert("Perfecto, elegiste la opción de películas o series, tu chaqueta está casi lista, nos contactaremos con vos para que nos cuentes los detalles y que tu chaqueta se vea como vos querés!")
-            console.log("Seleccionaste: Peliculas o series")
-            break;
+}else if (selection === "no"){
 
-        case "Un artista o banda":
-            alert ("Perfecto, elegiste la opción de Un artista o banda, tu chaqueta está casi lista, nos contactaremos con vos para que nos cuentes los detalles y que tu chaqueta se vea como vos querés!")
-            console.log("Seleccionaste: un artista o banda")
-            break;
-
-        case "Solo tipografia":
-            alert ("Perfecto, elegiste la opción de Solo tipografía, tu chaqueta está casi lista, nos contactaremos con vos para que nos cuentes los detalles y que tu chaqueta se vea como vos querés!")
-            console.log("Seleccionaste: Solo tipografía")
-            break;
-
-        case "Naturaleza":
-            alert ("Perfecto, elegiste la opción de Naturaleza, tu chaqueta está casi lista, nos contactaremos con vos para que nos cuentes los detalles y que tu chaqueta se vea como vos querés!")
-            console.log("Seleccionaste: Naturaleza")
-            break;
-
-        case "Psicodelica":
-            alert ("Perfecto, elegiste la opción de Psicodelica, tu chaqueta está casi lista, nos contactaremos con vos para que nos cuentes los detalles y que tu chaqueta se vea como vos querés!")
-            console.log("Seleccionaste: Psicodelica")
-            break;
-
-        case "Dibujos animados":
-            alert ("Perfecto, elegiste la opción de Dibujos animados, tu chaqueta está casi lista, nos contactaremos con vos para que nos cuentes los detalles y que tu chaqueta se vea como vos querés!")
-            console.log("Seleccionaste: Dibujos animados")
-            break;
-
-        case "Animales":
-            alert ("Perfecto, elegiste la opción de Animales, tu chaqueta está casi lista, nos contactaremos con vos para que nos cuentes los detalles y que tu chaqueta se vea como vos querés!")
-            console.log("Seleccionaste: Animales")
-            break;
-
-        case "Tengo mi dieño":
-            alert ("Perfecto, elegiste la opción de Tengo mi diseño, tu chaqueta está casi lista, nos contactaremos con vos para que nos cuentes los detalles y que tu chaqueta se vea como vos querés!")
-            console.log("Seleccionaste: Tengo mi diseño")
-            break;
-    
-        default:
-        alert ("Ingrese una opcion valida")
-        finalizandoCampera ()
-            break;
-    }
-
+    alert("Gracias por visitarnos! ♥");
 }
 
+while(selection != "no"){
 
-while (Bienvenida === "1"){
-    /* Dato libre solo para clg*/
-    crearCampera ()
-    finalizandoCampera ()
-    let numeroCelular = prompt ("Ingresa tu numero celular para que te contactemos")
-    console.log("numero de contacto: "+ numeroCelular)
-    let salida = prompt ("¿Desea diseñar otra chaqueta? si no lo desea escriba salir para finalizar")
-    if (salida === "salir"){
-        break
-    } else if (salida === "continuar"){
-        crearCampera ()
-    }
+    let idProducto = parseInt(prompt("Ingrese la opción de la campera que te gustaría comprar para agregarla al carrito"));
+    let price = 0;
+    let chaqueta = [];
+
+    if(idProducto === 1 || idProducto === 2 || idProducto === 3 || idProducto === 4 || idProducto === 5 ){
+        
+            switch(idProducto){
+
+            case 1:
+            chaqueta = "Campera Flower"
+            price = 35000;
+            break;
+            
+            case 2:
+            chaqueta = "Campera Frida"
+            price = 40000;
+            break; 
+
+            case 3:
+            chaqueta = "Campera Freddie"
+            price = 40000;
+            break;
+            
+            case 4:
+            chaqueta = "Campera Cat"
+            price = 45000;
+            break; 
+
+            case 5:
+            chaqueta = "Campera Love"
+            price = 45000;
+            break;
 
 
-    if (Bienvenida === "2"){
-    break
+            default:
+            break; 
+        }
+
+        let units = parseInt(prompt("Cuantas camperas querés agregar?"));
+        shoppingCart.push({idProducto, price, chaqueta, units})
+        console.log(shoppingCart);
+
+    }else{
+
+        alert("Esta campera no se enuentra!");
+
+    };
+
+    selection = prompt("Querés seguir comprando?")
+
+    while(selection === "no"){
+
+        shoppingCart.forEach((carritoFinal) => {
+
+            alert(`
+
+            Opción: ${carritoFinal.idProducto}
+            Producto: ${carritoFinal.chaqueta} 
+            Unidades: ${carritoFinal.units}
+            Total a pagar por producto: $${carritoFinal.units * carritoFinal.price}
+
+            `)
+        });
+
+        break;
     }
 }
+
+const precioTotal = shoppingCart.reduce((acumulador, totalProductos) => acumulador + totalProductos.price * totalProductos.units, 0);
+alert(`El total a pagar es: $${precioTotal}`);
